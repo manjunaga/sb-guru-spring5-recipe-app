592d32e2f6036f36aaef8f28bbfce2317bbcd9c5
Manjunatha Nagaraja <nmanjunatha@gmail.com>
2020-12-31 08:25:44 -0500

    Added querying db through spring

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,36 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+
 @Controller
 public class IndexController {
-	
-	@RequestMapping({"","/","/index"})
-	public String getIndexPage() {
-		return "index";
+
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitofmeasurerepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitofmeasurerepository;
 	}
 
+	@RequestMapping({ "", "/", "/index" })
+	public String getIndexPage() {
+		
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUomdescription("Teaspoon");
+		
+		System.out.println("Cat Id is " + categoryOptional.get().getId());
+		System.out.println("UOM Id is " + unitOfMeasureOptional.get().getId());
+		
+		return "index";
+	}
 }
